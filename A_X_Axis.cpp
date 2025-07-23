3354b07fecb3875148a446368ee75796c1062f68
Shibam Biswas <sbiswas2004in@gmail.com>
2025-07-23 22:06:53 +0530

    1986A - X Axis

---
+++ A_X_Axis.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long a, b, c;
+        cin >> a >> b >> c;
+
+        int res = 0;
+        int mid = a + b + c - max(a, max(b, c)) - min(a, min(b, c));
+        res += abs(mid - a) + abs(mid - b) + abs(mid - c);
+        cout << res << endl;
+    }
+    
+    return 0;
+}
