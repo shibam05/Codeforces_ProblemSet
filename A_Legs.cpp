28cea25b4de9af8ebc34255eb3c57365f2f414b6
Shibam Biswas <sbiswas2004in@gmail.com>
2025-07-23 21:04:40 +0530

    1996A - Legs

---
+++ A_Legs.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int cows = 0, chickens = 0;
+
+        if (n % 4 == 0) // only add cows to get minimum result
+        {
+            cows = n / 4;
+        }
+        else
+        {
+            cows = n / 4;
+            chickens = (n % 4) / 2;
+        }
+        
+        int res = cows + chickens;
+        cout << res << endl;
+    }
+
+    return 0;
+}
