325d4b672041afb5cddf8f7432a6a114a6ac0488
Shibam Biswas <sbiswas2004in@gmail.com>
2025-10-18 22:52:01 +0530

    1843C - Sum in Binary Tree

---
+++ C_Sum_in_Binary_Tree.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long int n;
+        cin >> n;
+        long long int res = 0;
+        while (n > 0)
+        {
+            res += n;
+            n /= 2;
+        }
+        cout << res << endl;
+    }
+    return 0;
+}
