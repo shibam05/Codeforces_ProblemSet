7dca7c9f9a44c3adc380937a520de907e43feb1c
Shibam Biswas <sbiswas2004in@gmail.com>
2025-12-07 20:43:57 +0530

    1559A - Mocha and Math

---
+++ A_Mocha_and_Math.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+
+        for (int i = 0; i < n; ++i)
+        {
+            cin >> arr[i];
+        }
+
+        int res = arr[0];
+
+        for (int i = 1; i < n; ++i)
+        {
+            res = res & arr[i];
+        }
+        cout << res << endl;
+    }
+
+    return 0;
+}
