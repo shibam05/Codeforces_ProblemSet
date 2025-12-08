2de7520f09a05cff3117f4cd8fd075e9e7b1523f
Shibam Biswas <sbiswas2004in@gmail.com>
2025-12-08 17:42:33 +0530

    959A - Mahmoud and Ehab and the even-odd game

---
+++ A_Mahmoud_and_Ehab_and_the_even_odd_game.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    if (n % 2 == 1)
+        cout << "Ehab" << endl;
+    else
+        cout << "Mahmoud" << endl;
+    return 0;
+}
