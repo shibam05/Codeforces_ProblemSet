be7b56c935ff70b937bf07d8b3a765f20e2ac83a
Shibam Biswas <sbiswas2004in@gmail.com>
2026-01-29 21:31:27 +0530

    2184A - Social Experiment

---
+++ A_Social_Experiment.py
@@ -0,0 +1,12 @@
+t = int(input())
+for _ in range(t):
+    n = int(input())
+
+    if n == 2:
+        print(2)
+    elif n == 3:
+        print(3)
+    elif n % 2 == 0:
+        print(0)
+    else:
+        print(1)
